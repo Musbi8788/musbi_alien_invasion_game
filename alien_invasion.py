3c140646859642472142aadb85d9561b5d59b724
Musbi <musbi700@gmail.com>
2025-06-22 22:36:01 +0100

    added ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,8 +16,9 @@ class AlienInvasion():
 
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption(self.settings.game_title)
-        
+
         self.ship = Ship(self)
+
 
     def run_game(self):
         """Start the main loop for the game
@@ -27,6 +28,10 @@ class AlienInvasion():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # draw the screen during each pass through loop
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently display visible
             pygame.display.flip()
