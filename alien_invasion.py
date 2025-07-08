099168c4422db6132580525c56a9f07ff1e2fe63
Musbi <musbi700@gmail.com>
2025-07-08 23:46:40 +0100

    alien fleet moving right

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,6 +47,7 @@ class AlienInvasion():
             self.ship.update()
             self.bullets.update()
             self._update_bullets() 
+            self._update_aliens()
             self._update_screen()
         
     def _check_events(self):
@@ -135,6 +136,11 @@ class AlienInvasion():
 
         alien.rect.y  = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet
+        """
+        self.aliens.update()
         
 
     def _update_screen(self):
