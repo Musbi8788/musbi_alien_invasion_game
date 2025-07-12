114c06f41b29e12c83c8746efca672cd2e90c37d
Musbi <musbi700@gmail.com>
2025-07-12 20:08:08 +0100

    bullets speed adjusted to 1.5

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ class Settings():
         """
 
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 300 # testing mode
+        self.bullet_speed = 1.5
+        self.bullet_width = 3 # testing mode
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
