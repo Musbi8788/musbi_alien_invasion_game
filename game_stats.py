97bbb4bc6bfde2ddf9b587272072cbbae859e69b
Musbi <musbi700@gmail.com>
2025-07-15 22:36:49 +0100

    first commit

---
+++ game_stats.py
@@ -0,0 +1,11 @@
+class GameStats:
+    """Track statistics for Alien Invasion"""
+
+    def __init___(self, ai_game):
+        """Initialize statistics"""
+        self.settings = ai_game.settings
+        self.rest_stats()
+
+    def rest_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
