d3df17746c43401fcc338b6742e08b3c6685c0a7
Musbi <musbi700@gmail.com>
2025-07-15 23:24:13 +0100

    updated

--- game_stats.py
+++ game_stats.py
@@ -1,11 +1,12 @@
 class GameStats:
     """Track statistics for Alien Invasion"""
 
-    def __init___(self, ai_game):
+    def __init__(self, ai_game):
         """Initialize statistics"""
         self.settings = ai_game.settings
         self.rest_stats()
 
-    def rest_stats(self):
+    def rest_stats(self,):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
+        
