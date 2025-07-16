8f5fe6bcffa0cc59f9ff6074141ec97ff70869d4
Musbi <musbi700@gmail.com>
2025-07-16 08:52:36 +0100

    updated

--- game_stats.py
+++ game_stats.py
@@ -1,10 +1,12 @@
 class GameStats:
     """Track statistics for Alien Invasion"""
-
+    
     def __init__(self, ai_game):
         """Initialize statistics"""
         self.settings = ai_game.settings
         self.rest_stats()
+        # Start Alien Invasion in an active state
+        self.game_active = True 
 
     def rest_stats(self,):
         """Initialize statistics that can change during the game."""
