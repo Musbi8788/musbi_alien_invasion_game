7859c9e82d7011b5e1227de2fa227fa852451af6
Musbi <musbi700@gmail.com>
2025-07-19 01:28:18 +0100

    ships left on the borad

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,15 @@
 import pygame.font
+from pygame.sprite import Group # We are importing a group of ship
+
+from ship import Ship # will be change to life later
+
 
 class Scoreboard:
     """A class to report scoring information"""
 
     def __init__(self, ai_game):
         """Initialize the scorekeeping attributes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
@@ -17,11 +22,37 @@ class Scoreboard:
         # prepare the initail score images 
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
+        self.prep_ships() # will be change to life
+
+    def prep_ships(self):
+        """Show how many ships left."""
+        self.ships = Group() # set the ships group
+        for ship_number in range(self.stats.ships_left): # display the ships left
+            ship = Ship(self.ai_game) # create an instance of ship
+            # Position the ships left to the left top screen
+            ship.rect.x = 10 + ship_number * ship.rect.width 
+            ship.rect.y = 10
+            self.ships.add(ship) # add the ships 
+            """this make the ship appear next to each other (-_-)  (-_-)  (-_-) """
+
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level_str = "Level: " + str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color) # generate the level text image
+
+        # Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10 # position the level under the score
+
 
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1) # round the number eg 100000 to 10,000
-        score_str = "{:,}".format(rounded_score) # add comma to the score 
+        # add comma to the score
+        score_str = "Score: " + "{:,}".format(rounded_score)
 
         self.score_image = self.font.render(score_str, True , self.text_color, self.settings.bg_color)
 
@@ -33,7 +64,7 @@ class Scoreboard:
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, - 1)
-        self.high_score_str = "{:,}".format(high_score)
+        self.high_score_str = "High Score: " + "{:,}".format(high_score)
         self.high_score_image = self.font.render(self.high_score_str, True, self.text_color, self.settings.bg_color)
 
         # Center the high score at the top of the screen.
@@ -53,3 +84,5 @@ class Scoreboard:
         """Draw the score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen) # Draw the ships
