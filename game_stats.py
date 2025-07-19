2f784efdba3931f24d4acc327e95d8c8b98a8bb9
Musbi <musbi700@gmail.com>
2025-07-19 09:46:57 +0100

    High score store added

--- game_stats.py
+++ game_stats.py
@@ -9,7 +9,14 @@ class GameStats:
         self.game_active = False
 
         # High score should never be reset.
-        self.high_score = 0
+        try:
+            with open("high_score.txt", "r") as content:
+                all_time_high_score = content.read()
+                
+            self.high_score = int(all_time_high_score)
+
+        except FileNotFoundError:
+            self.high_score = 0
 
     def rest_stats(self,):
         """Initialize statistics that can change during the game."""
